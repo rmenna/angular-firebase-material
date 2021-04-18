135e459521d4a261d653e83e2da87a55ffbf0797
Rodrigo Menna <rmenna.barroso@gmail.com>
2021-04-18 19:36:41 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyB6mvfc3WQagngBkcwWyQjYsJNzwlzWOkc',
-    authDomain: 'angular-tasks-7ed46.firebaseapp.com',
-    projectId: 'angular-tasks-7ed46',
-    storageBucket: 'angular-tasks-7ed46.appspot.com',
-    messagingSenderId: '388456791591',
-    appId: '1:388456791591:web:f5ebb22ac3a9b165155802',
-    measurementId: 'G-0M7MZ1LB75'
+    // colocar a sua configuração do firebase aqui
   }
 };
